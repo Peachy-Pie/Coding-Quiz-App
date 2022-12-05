cf5a3cd1ba031015e04af24535df6ce21ef4c92d
Peachy-Pie <patrick.e.straw@gmail.com>
2022-12-05 15:29:34 -0800

    added multiple functions to display questions/check if answer is correct/scoring and timer created new variables for score time alotted and time taken away.

--- script.js
+++ script.js
@@ -7,52 +7,139 @@
 // when no more questions call function to end game
 // when timer is over calls function that ends game
 
-const startBtn = document.getElementById('startBtn')
-const questionsContainerEle = document.getElementById('questionContainer')
-const preQuestionContent = document.getElementById('preQuiz')
-const questionEle = document.getElementById('question')
-const answerBtnEle = document.getElementById('answerBtns')
+const startBtn = document.querySelector('#startBtn')
+const questionsContainerEle = document.querySelector('#questionContainer')
+const preQuestionContent = document.querySelector('#preQuiz')
+const questionEle = document.querySelector('#question')
+const answersEle = document.querySelector('#answers')
+const timerEle = document.querySelector('#timer')
 
-startBtn.addEventListener('click', startQuiz)
+const inputScoreEl = document.querySelector("#inputScore");
+const initialsEl = document.querySelector("#initials");
+const submitInitialsBtnEl = document.querySelector("#submitInitials");
+const userScoreEl = document.querySelector("#score");
 
-let shuffledQuestions, currentQuestionIndex
+const highScoresEl = document.querySelector("#highScores");
+const scoresEl = document.querySelector("#scores");
+const goBackBtnEl = document.querySelector("#goBack");
+const clearScoresBtnEl = document.querySelector("#clearScores");
+const viewHScoresBtnEl = document.querySelector("#viewHScores");
 
-function startQuiz() {
-    startBtn.classList.add('hide')
-    preQuestionContent.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() -.5)
-    currentQuestionIndex = 0
-    questionsContainerEle.classList.remove('hide')
-    setNextQuestion()
+let score = 0;
+let currentQuestion = 0;
+let highScores = [];
+let interval;
+let timeGiven = 120;
+let timeGone = 0;
+
+startBtn.addEventListener('click', function () {
+    hide(preQuestionContent)
+    timerStart()
+    showQuestion()
+    show(questionsContainerEle)
+})
+
+answersEle.addEventListener("click", function (e) {
+    if (e.target.matches("button")) {
+        checkAnswer(e.target);
+        setNextQuestion();
+    }
+});
+
+function timerStart () {
+    timerEle.textContent = timeGiven;
+    interval = setInterval(function () {
+        timeGone++;
+        timerEle.textContent = timeGiven - timeGone;
+        if (timeGone >= timeGiven) {
+            currentQuestion = questions.length;
+            setNextQuestion();
+        }
+    }, 1000);
 }
+
+function stopTimer() {
+    clearInterval(interval)
+}
+
 
 function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+    showQuestion();
+    } else {
+        stopTimer();
+        if((timeGiven - timeGone) > 0)
+            score += (timeGiven - timeGone);
+        userScoreEl.textContent = score;
+        hide(questionsContainerEle)
+        show(inputScoreEl)
+        timerEle.textContent = 0;
+    }
 }
 
-function showQuestion(question) {
-    questionEle.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        button.addEventListener('click', selectAnswer)
-        answerBtnEle.appendChild(button)
-    })
+function checkAnswer(answer) {
+    if (questions[currentQuestion].answer == questions[currentQuestion].choices[answer.id]) {
+        score += 5;
+        displayMessage("Correct!");
+    }
+    else {
+        timeGone += 10;
+        displayMessage("Wrong...");
+    }
 }
 
-function selectAnswer(e) {
-    const selectedButton = e.target
+function displayMessage(m) {
+    let messageHr = document.createElement("hr");
+    let messageEl = document.createElement("div");
+    messageEl.textContent = m;
+    document.querySelector(".container").appendChild(messageHr);
+    document.querySelector(".container").appendChild(messageEl);
+    setTimeout(function () {
+            messageHr.remove();
+            messageEl.remove();
+    }, 2000);
+
 }
+
+
+function showQuestion() {
+    questionEle.textContent = questions[currentQuestion].title;
+    for (i = 0; i < answersEle.children.length; i++) {
+        answersEle.children[i].textContent = questions[currentQuestion].choices[i]
+    }
+}
+
+//hides element
+function hide(element) {
+    element.style.display = "none";
+}
+
+//displays element
+function show(element) {
+    element.style.display = "block";
+}
+
+function reset() {
+    score = 0;
+    currentQ = 0;
+    secondsElapsed = 0;
+    timerEle.textContent = 0;
+}
+
+viewHScoresBtnEl.addEventListener("click", function () {
+    hide(preQuestionContent);
+    hide(questionsContainerEle);
+    hide(inputScoreEl);
+    renderHighScores();
+    stopTimer();
+    reset();
+});
 
 const questions = [
     {
-        question: 'what is 2 + 2',
-        answers: [
-            { text: '4', correct: true },
-            { text: '22', correct: false },
-            { text: '24', correct : false },
-            { text: '9001', correct: false },
-        ]
-    },
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    }
 ]
